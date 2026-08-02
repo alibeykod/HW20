be996aa4df4041737d6d50e5bb4e7103f1a8e23c
beykod <alirafibeyk@gmail.com>
2026-08-02 17:36:15 +0330

    feat: Add inTxReturn method to the HibernateUtil

--- src/main/java/ir/maktabsharif/util/HibernateUtil.java
+++ src/main/java/ir/maktabsharif/util/HibernateUtil.java
@@ -2,7 +2,10 @@ package ir.maktabsharif.util;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+import java.util.function.Function;
 
 public class HibernateUtil {
     private static final String persistenceUnit = "postgres-pu";
@@ -16,6 +19,27 @@ public class HibernateUtil {
             emf = Persistence.createEntityManagerFactory(persistenceUnit);
         }
         return emf;
+    }
+
+    public static EntityManager em() {
+        return emf().createEntityManager();
+    }
+
+    public static <T> T inTxReturn(Function<EntityManager, T> operation) {
+        EntityManager em = em();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            T result = operation.apply(em);
+            tx.commit();
+            return result;
+        } catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
     }
 
 }
