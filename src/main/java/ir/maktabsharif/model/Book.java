97ea846a37f99d20242e46cdaf2207ea590eea44
beykod <alirafibeyk@gmail.com>
2026-08-02 13:34:59 +0330

    feat: Change Table Name To The Books

--- src/main/java/ir/maktabsharif/model/Book.java
+++ src/main/java/ir/maktabsharif/model/Book.java
@@ -5,17 +5,24 @@ import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Table(name = "Books")
+
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID" , unique = true , nullable = false)
+    @Column(name = "ID", unique = true, nullable = false)
     private UUID id;
+
     @Enumerated(value = EnumType.STRING)
     private BookStockStatus bookStockStatus;
-    @Column(nullable = false )
+
+    @Column(nullable = false)
     private String title;
+
     private int publicationYear;
+
     private double price;
+
     @Embedded
     private PublisherAddress publisherAddress;
 
